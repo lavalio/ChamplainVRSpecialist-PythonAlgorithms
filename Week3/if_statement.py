bcacaa0616bd40785e0f61301e3b3ae0e3489d9c
lavalio <jz2326@gmail.com>
2018-11-13 11:52:56 -0500

    Hi

--- Week3/if_statement.py
+++ Week3/if_statement.py
@@ -5,8 +5,21 @@ if number == 42 :
 else:
     print("not match")
 
+number = 412
 
-if number == 412 :
+if number == 42 :
     print("matched")
 else:
     print("not match")
+
+number == 412
+
+if number > 42 :
+    print("The number "+str(number)+ " is greater than 42")
+else:
+    print("The number "+str(number)+ " is leaser than 42")
+
+myvalue = 34
+
+if myvalue > 20 and myvalue != 33 :
+    print("The number is above 20 and it's not 33 ")
