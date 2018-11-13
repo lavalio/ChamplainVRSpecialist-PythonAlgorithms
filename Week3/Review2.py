4ad0a997dad0ed4b2fffbcf5dc51db566e327f96
lavalio <jz2326@gmail.com>
2018-11-13 09:58:13 -0500

    Hi

---
+++ Week3/Review2.py
@@ -0,0 +1,9 @@
+mString = input(">")
+
+print(mString.upper())
+print(mString.title())
+print(mString.lower())
+
+print("The length of the string is " + str(len(mString)))
+
+print(mString.replace("!","."))
