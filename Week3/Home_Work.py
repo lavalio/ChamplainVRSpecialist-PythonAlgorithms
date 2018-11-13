534f66b8295ab669757bfa7ea7a42456f19a7cea
lavalio <jz2326@gmail.com>
2018-11-13 12:09:18 -0500

    Hi

--- Week3/Home_Work.py
+++ Week3/Home_Work.py
@@ -1,5 +1,8 @@
 for a in range(100):
-    if a % 3 == 0:
-        print("Fizz")
-    elif a % 5 == 0:
+    if (a % 3 == 0) and (a % 5 != 0):
+        print(str(a) + " :Fizz")
+    elif (a % 3 != 0) and (a % 5 == 0):
+        print(str(a) +" :Buzz")
+    else:
+        print(str(a) +" :FizzBuzz")
 
