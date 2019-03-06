b6b05081b8788d50e7d3084b004d34f19db98aff
lavalio <jz2326@gmail.com>
2019-03-06 18:45:01 -0500

    Asignment

--- week16/assignment/the_basics_1.py
+++ week16/assignment/the_basics_1.py
@@ -1,31 +1,56 @@
 import string
+
 Input_list = [6,"g",5,"b","$","c",7,26,5,"z","!","P6","6","P"]
 
 
-def is_number(s):
+def is_number(n):
     try:
-        float(s) # for int, long and float
+        float(n)   # Type-casting the string to `float`.
+                   # If string is not a valid `float`,
+                   # it'll raise `ValueError` exception
     except ValueError:
-        try:
-            complex(s) # for complex
-        except ValueError:
-            return False
-
+        return False
     return True
 
-#for c in string.punctuation:
-#    print("[" + c + "]")
+def is_punctuation(s):
+    for item in string.punctuation:
+        if s == item:
+            return True
+        return False
+
+def odd_even_number(num):
+    if (int(num) % 2) == 0:
+        return "an even number"
+    return "an odd number"
+
+def max_number():
+    max_value = 0
+    for c in Input_list:
+        if is_number(c):
+            if int(c) > max_value:
+                max_value = c
+    return max_value
+
+def decide_max(c,max):
+    if int(c) < max:
+        return "is not the highest number in the list"
+    return "is the highest number  in the list"
+
 
 
 i = 0
+max = max_number()
+
 for c in Input_list:
+
     if is_number(c):
-        print("Elemnet{}: {} is a number".format(i,c))
+        print("Elemnet{}: {} is a number.".format(i,c), "This is {}".format(odd_even_number(c)))
+        print("This is {}".format(decide_max(c, max))) # Is this the highest number  in the list ?
 
     elif c.isalpha():
         print("Elemnet{}: {} is a character".format(i, c))
 
-    elif c == string.punctuation:
+    elif is_punctuation(c):
         print("Elemnet{}: {} is a punctuation".format(i, c))
 
     i+=1
